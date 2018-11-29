3fb4d336f07a3cccf9bfb8825cb330062f4a0d8a
Girlesa María Buitrago <girlesa.buitrago@transmilenio.gov.co>
2018-11-29 12:33:14 -0500

    agregación de un panel con el nombre del reporte

---
+++ build/generated-sources/ap-source-output/entities/HistoricoCuentaBuses_.java
@@ -0,0 +1,28 @@
+package entities;
+
+import java.math.BigInteger;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-29T12:13:23")
+@StaticMetamodel(HistoricoCuentaBuses.class)
+public class HistoricoCuentaBuses_ { 
+
+    public static volatile SingularAttribute<HistoricoCuentaBuses, Double> suma;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, BigInteger> conteoBusesTipologia;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> rutaCom;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> linea;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> tipologia;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> rc;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> zona;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, Date> fecha;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> tipodia;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, Integer> cuenta;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> mes;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, Integer> id;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> operador;
+    public static volatile SingularAttribute<HistoricoCuentaBuses, String> anio;
+
+}
